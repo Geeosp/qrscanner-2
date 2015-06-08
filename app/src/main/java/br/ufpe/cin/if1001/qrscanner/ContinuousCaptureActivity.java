5e91ab061d7f31eb798e1b2b1baf95ad599c3291
geovane <geeo.sp@gmail.com>
2015-06-08 16:45:46 -0300

    Initial commit

---
+++ app/src/main/java/br/ufpe/cin/if1001/qrscanner/ContinuousCaptureActivity.java
@@ -0,0 +1,95 @@
+package br.ufpe.cin.if1001.qrscanner;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.zxing.ResultPoint;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.CompoundBarcodeView;
+
+//import com.google.zxing.client.android.R;
+
+
+
+import java.util.List;
+
+/**
+ * This sample performs continuous scanning, displaying the barcode and source image whenever
+ * a barcode is scanned.
+ */
+public class ContinuousCaptureActivity extends Activity {
+    private static final String TAG = ContinuousCaptureActivity.class.getSimpleName();
+    private CompoundBarcodeView barcodeView;
+
+    private BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            if (result.getText() != null) {
+                barcodeView.setStatusText(result.getText());
+                // aqui eh onde pega o texto
+            }
+            //Added preview of scanned barcode
+            ImageView imageView = (ImageView) findViewById(R.id.barcodePreview);
+            imageView.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW));
+        }
+
+        @Override
+        public void possibleResultPoints(List<ResultPoint> resultPoints) {
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        IntentIntegrator integrator = new IntentIntegrator(this);
+//        integrator.setPrompt("Aproxime a câmera do código QR");
+//        integrator.addExtra("SCAN_MODE", "QR_CODE_MODE");
+//        integrator.setOrientationLocked(false);
+//        integrator.initiateScan();
+
+        setContentView(R.layout.activity_continuous_capture);
+
+        barcodeView = (CompoundBarcodeView) findViewById(R.id.barcode_scanner);
+        barcodeView.decodeContinuous(callback);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        barcodeView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        barcodeView.pause();
+    }
+
+    public void pause(View view) {
+        barcodeView.pause();
+    }
+
+    public void resume(View view) {
+        barcodeView.resume();
+    }
+
+    public void triggerScan(View view) {
+        barcodeView.decodeSingle(callback);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return barcodeView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+}
